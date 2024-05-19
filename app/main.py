9d54e0608fce199ecce7279e1cd429ed5ddd3d0e
rdcheadle3 <robert.cheadle01@gmail.com>
2024-05-19 11:26:31 -0400

    stage 3 first attempt

--- app/main.py
+++ app/main.py
@@ -3,17 +3,24 @@ import socket
 
 
 def main():
-
+    # accept connection
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept()
     print(f"connection accepted {client_address}")
-
+    
+    # read request
     request = client_socket.recv(1024)
     print("request recieved")
     print(request.decode("utf-8"))
 
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    client_socket.sendall(response.encode("utf-8"))
+    # get path
+    request_line = request.splitlines()[0]
+    path = request_line.split()[1]
+    
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
     client_socket.close()
     server_socket.close()
