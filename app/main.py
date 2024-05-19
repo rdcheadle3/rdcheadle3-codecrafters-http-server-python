1d11bc82d6c6081a85c9d6a268b4459cc5c70767
rdcheadle3 <robert.cheadle01@gmail.com>
2024-05-19 10:59:31 -0400

    stage 2 second attempt

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
     client_socket, client_address = server_socket.accept()
+    print(f"connection accepted {client_address}")
 
     request = client_socket.recv(1024)
+    print("request recieved")
+    print(request.decode("utf-8"))
 
     response = "HTTP/1.1 200 OK\r\n\r\n"
     client_socket.sendall(response.encode("utf-8"))
